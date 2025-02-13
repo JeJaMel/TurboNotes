a59151af1e14e71c80548b3a66eb7784cbc1d60e
JeJaMel <jjmelendezb15@gmail.com>
2025-02-12 21:13:28 -0400

    feat: Enhanced GoogleLogin

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,17 @@
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
+import ForgotPassword from "../components/Auth/ForgotPassword";
+import GoogleLogin from "../components/Auth/GoogleLogin";
 
 const Auth = () => {
   return (
     <div>
       <h1>Welcome to Auth!</h1>
       <Login />
+      <ForgotPassword />
       <Register />
+        <GoogleLogin buttonText="Sign in with google" />
+        <GoogleLogin/>
     </div>
   );
 };
