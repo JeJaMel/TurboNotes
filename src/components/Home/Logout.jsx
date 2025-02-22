97517aa7fdfec23c52293eb423309e4db506b852
JeJaMel <jjmelendezb15@gmail.com>
2025-02-21 23:21:11 -0400

    feat: Update Logout

--- src/components/Home/Logout.jsx
+++ src/components/Home/Logout.jsx
@@ -2,6 +2,7 @@ import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase/firebase";
 import styles from "../../css/Home/Logout.module.css";
+import { BiPowerOff } from "react-icons/bi";
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -16,7 +17,14 @@ const Logout = () => {
     }
   };
 
-  return <button onClick={handleLogout}>Logout</button>;
+  return (
+    <div className={styles.logoutButtonContainer}>
+      <button className={styles.logoutButton} onClick={handleLogout}>
+        <BiPowerOff className={styles.logoutIcon} />
+        <span className={styles.logoutTitle}>Logout</span>
+      </button>
+    </div>
+  );
 };
 
 export default Logout;
